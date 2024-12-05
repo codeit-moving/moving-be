4626ce8741d8c66441bffeeb470832abf1359ff8
mini-1018 <anjaemin1018@gmail.com>
2024-12-05 18:57:28 +0900

    :bug: [기능수정] jwt-optional

--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -22,19 +22,19 @@ passport.use(
       error.data = {
         message: "토큰이 존재하지 않습니다.",
       };
-      return done(error, false);
+      return done(error, false); // 토큰이 없을 때 에러 반환
     }
 
     try {
       const decoded = jwt.verify(token, JWT_SECRET);
-      return done(null, decoded);
+      return done(null, decoded); // 토큰이 있고 유효하면 유저 정보 반환
     } catch (err) {
       const error: CustomError = new Error("Unauthorized");
       error.status = 401;
       error.data = {
         message: "유효하지 않은 토큰입니다.",
       };
-      return done(error, false);
+      return done(error, false); // 토큰이 있지만 유효하지 않으면 false 반환
     }
   })
 );
@@ -44,13 +44,14 @@ passport.use(
   new Strategy(async (req, done) => {
     const token = req.cookies["accessToken"];
     if (!token) {
+      //토큰이 없어도 통과
       return done(null, null);
     }
     try {
-      const decoded = jwt.verify(token, JWT_SECRET);
+      const decoded = jwt.verify(token, JWT_SECRET); // 토큰이 있다면 검증 후 유저 정보 반환
       return done(null, decoded);
     } catch (error) {
-      return done(null, null);
+      return done(null, false); // 토큰이 있지만 유효하지 않으면 false 반환
     }
   })
 );
