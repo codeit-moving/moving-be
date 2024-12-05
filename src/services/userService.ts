10ae657186d750651aaa018e061af4e452707203
mini-1018 <anjaemin1018@gmail.com>
2024-12-05 14:37:22 +0900

    :art: [기능개선] 패스워드 검증 순서 변경

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -11,20 +11,20 @@ interface UpdateUser {
 const updateUser = async (userId: number, updateData: UpdateUser) => {
   const user = await userRepository.findById(userId);
   if (updateData.newPassword && updateData.currentPassword) {
-    if (updateData.currentPassword === updateData.newPassword) {
-      const error: CustomError = new Error("Conflict");
-      error.status = 409;
-      error.data = {
-        message: "기존 비밀번호와 동일합니다.",
-      };
-      throw error;
-    }
-
     if (user!.password !== updateData.currentPassword) {
       const error: CustomError = new Error("Unauthorized");
       error.status = 401;
       error.data = {
         message: "현재 비밀번호가 일치하지 않습니다.",
+      };
+      throw error;
+    }
+
+    if (updateData.currentPassword === updateData.newPassword) {
+      const error: CustomError = new Error("Conflict");
+      error.status = 409;
+      error.data = {
+        message: "기존 비밀번호와 동일합니다.",
       };
       throw error;
     }
