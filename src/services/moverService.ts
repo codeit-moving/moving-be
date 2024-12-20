8225993613eb1664ceb1aacd6e4b43ca8f0579e1
twh <owootak@naver.com>
2024-12-20 13:45:13 +0900

    ♻️ [리팩토링] : 기사관련 로직 리팩토링

--- src/services/moverService.ts
+++ src/services/moverService.ts
@@ -5,6 +5,7 @@ import processMoversData from "../utils/mover/processMoverData";
 import RatingResult from "../utils/interfaces/mover/ratingResult";
 import imageRepository from "../repositorys/imageRepository";
 import { uploadFile } from "../utils/s3.utils";
+import getRatingsByMoverIds from "../utils/mover/getRatingsByMover";
 
 interface queryString {
   order: string;
@@ -236,46 +237,6 @@ const getMover = async (moverId: number) => {
   const processedMover = await processMoversData(null, mover, ratingsByMover);
 
   return processedMover[0];
-};
-
-//평점 조회
-const getRatingsByMoverIds = async (moverIds: number | number[]) => {
-  const moverIdArray = Array.isArray(moverIds) ? moverIds : [moverIds];
-
-  const ratings = await moverRepository.getRatingsByMoverIds(moverIdArray);
-
-  // moverId별로 그룹화
-  const ratingsByMover = moverIdArray.reduce((acc, moverId) => {
-    acc[moverId] = {
-      totalCount: 0,
-      totalSum: 0,
-      "1": 0,
-      "2": 0,
-      "3": 0,
-      "4": 0,
-      "5": 0,
-      average: 0,
-    };
-    return acc;
-  }, {} as Record<number, RatingResult>);
-
-  // 각 rating 데이터 처리
-  ratings.forEach((rating) => {
-    const moverRating = ratingsByMover[rating.moverId];
-    moverRating.totalCount += rating._count.rating;
-    moverRating.totalSum += rating.rating * rating._count.rating;
-    moverRating[`${rating.rating}`] = rating._count.rating;
-  });
-
-  // 평균 계산 및 totalSum 제거
-  Object.values(ratingsByMover).forEach((rating) => {
-    rating.average =
-      Math.round((rating.totalSum / rating.totalCount) * 10) / 10;
-    const { totalSum, ...returnResult } = rating;
-    return returnResult;
-  });
-
-  return ratingsByMover;
 };
 
 const updateMoverProfile = async (
